c3ad8b2c1e41f3e657aa446b59c857a2b9d1d866
apmoon <ohappy0206@naver.com>
2024-05-28 12:47:47 +0900

    apmoon) 프로젝트 기본 세팅 240528 12:47

---
+++ src/test/java/mgh/oneroommate/OneRoomMateApplicationTests.java
@@ -0,0 +1,13 @@
+package mgh.oneroommate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneRoomMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
